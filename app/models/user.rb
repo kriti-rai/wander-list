174e96364a885cd37f02d7c50ab639b31c085866
Kriti Rai <kriti.rai@gmail.com>
2018-06-11 06:05:19 +0000

    Added methods to create slugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :username, :email, :password, presence: true
+
+  def slug
+    if self.username.include?(" ")
+      self.username.downcase.gsub(" ", "-")
+    else
+      self.username.downcase
+    end
+  end
+
+  def self.find_by_slug(slugified_name)
+    self.all.detect{|user| user.slug == slugified_name}
+  end
 end
