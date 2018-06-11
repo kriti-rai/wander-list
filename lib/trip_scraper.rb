477409c5b435a4a2fd5abf61812ff2b0586eb34f
Kriti Rai <kriti.rai@gmail.com>
2018-06-11 19:33:00 +0000

    Added scrape_page method

--- lib/trip_scraper.rb
+++ lib/trip_scraper.rb
@@ -1,9 +1,21 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class TripScraper
   #scrapes a list of destination from a website
   #website http://time.com/5050064/travel-and-leisure-places-to-travel-2018/
   #each trip is an instance of Trip class
 
+  def scrape_page
+    trips = []
 
-  def initialize
+    doc = Nokogiri::HTML(open('http://www.bbc.com/travel/destinations'))
+    container = doc.css('div.primary-content')
+    title = container.css('.nested-list li a').text
+    binding.pry
+    # doc.css('class-selector').map do |trip|
+    #   trip_
   end
 end
+TripScraper.new.scrape_page
