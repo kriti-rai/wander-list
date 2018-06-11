feb4033e8bf0645a8faad26709e1db2bb8275324
Kriti Rai <kriti.rai@gmail.com>
2018-06-11 18:41:05 +0000

    Added initialize method and comments

--- lib/trip_scraper.rb
+++ lib/trip_scraper.rb
@@ -1,2 +1,9 @@
 class TripScraper
+  #scrapes a list of destination from a website
+  #website http://time.com/5050064/travel-and-leisure-places-to-travel-2018/
+  #each trip is an instance of Trip class
+
+
+  def initialize
+  end
 end
