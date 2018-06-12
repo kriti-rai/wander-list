36b3eac4ca12eace2a3057761350aa1aea5ea4ae
Kriti Rai <kriti.rai@gmail.com>
2018-06-12 04:51:06 +0000

    Created create_from_collection class mehod

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,11 @@ class Trip < ActiveRecord::Base
   has_many :board_trips
   has_many :boards, through: :board_trips
   has_many :users, through: :boards
+
+  def self.create_from_collection
+    collection = TripScraper.new.scrape_page
+    collection.each do |trip_hash|
+      self.create(trip_hash)
+    end
+  end
 end
