3a1f08710ca9f0a1ad459dd9d222c6ddd525b515
Kriti Rai <kriti.rai@gmail.com>
2018-06-12 03:01:15 +0000

    Added url column to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_061742) do
+ActiveRecord::Schema.define(version: 2018_06_12_025858) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_06_10_061742) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
